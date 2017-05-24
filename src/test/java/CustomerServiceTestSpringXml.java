090b7ba5cf0b9ad7a70a9a7a760bdb55a44f15a4
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2017-05-24 16:42:01 +0800

    Fix bug on test case

--- src/test/java/CustomerServiceTestSpringXml.java
+++ src/test/java/CustomerServiceTestSpringXml.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertNotNull;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Configuration
-@ImportResource(locations = "classpath:/customerservice-test-context.xml")
+@ContextConfiguration(locations = "classpath:/customerservice-test-context.xml")
 public class CustomerServiceTestSpringXml {
 
   @Autowired
